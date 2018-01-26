7d03f1528f94d8cbf16e8fdd6f396dc746616b5d
LiKe Zhang <like.zhang@tmx.com>
2018-01-26 07:58:57 -0500

    add waring msg

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -11,8 +11,11 @@ import { UserService } from '../shared/user.service';
 })
 export class FeedbackComponent implements OnInit {
   private _success = new Subject<string>();
+  private _fail = new Subject<string>();
+
   staticAlertClosed = false;
   successMessage: string;
+  failMessage: string;
     public username:string;
     public email:string;
     public phone:string;
@@ -24,20 +27,41 @@ export class FeedbackComponent implements OnInit {
     setTimeout(() => this.staticAlertClosed = true, 10000);
     this._success.subscribe((message) => this.successMessage = message);
     debounceTime.call(this._success, 3000).subscribe(() => this.successMessage = null);
+
+    this._fail.subscribe((message) => this.failMessage = message);
+    debounceTime.call(this._fail, 3000).subscribe(() => this.failMessage = null);
   }
 
   public changeSuccessMessage() {
     this._success.next(`Feedback successfully send.`);
   }
 
+  public changeFailMessage() {
+    this._fail.next(`invalid Feedback field.`);
+  }
+
     onSubmitFeedback(){
       let self = this;
+
+      if(self.hasSpecialChar(this.message)||self.hasSpecialChar(this.username)||
+          self.hasSpecialChar(this.email)||self.hasSpecialChar(this.phone)){
+          self.changeFailMessage();
+          return;
+      }
+
       self.changeSuccessMessage();
+
+
 
       this.userServ.postFeedback(this.username, this.email, this.phone, this.message).subscribe(
           (data) => {
             let k = data;
           },
           (err)=>{});
+    }
+
+    hasSpecialChar(v){
+        var format = /[!#$%^&*()+\=\[\]{};':"\\|,.<>\/?]/;
+        return format.test(v);
     }
 }
