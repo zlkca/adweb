44064f5fc1151fe0441730192be0602e93528624
zlkca <likzhang@gmail.com>
2018-01-27 12:09:18 -0500

    add google analytics to the index page

--- server/config.js
+++ server/config.js
@@ -19,7 +19,7 @@ module.exports = {
 	dbPassword:'',
 	dbPort: '27017',
 	apiUrl: '/api',
-	ALLOW_ORIGIN:"http://localhost:5000",
+	ALLOW_ORIGIN:"http://yocompute.com:5000",
 	sessionPrefix: project,
 	sessionExpiry: expiry, // milliseconds
 	//'imageRoot' : imgRoot,
