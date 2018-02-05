a87dd36028ceb923d4c6a54ee30fd219f1f2a938
zlkca <likzhang@gmail.com>
2018-02-05 11:02:48 -0500

    reuse components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,6 @@
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    title: string = 'YoCompute Inc.';
-    lat: number = 43.643726;
-    lng: number = -79.392305;
-    zoom:number = 13;
 
     constructor() { }
 
